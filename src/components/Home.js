8d7f8a727aefaa776b350ba3b6da99784cbb4b6b
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 19:08:58 -0400

    Import signup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Login from './Login';
 import Logout from './Logout.js';
+import Signup from './Signup.js';
 
 const Home = ({}) => (
-  <div>Welcome, please log in or sign up</div>
+  <div>
+    <h2>Welcome, please log in or sign up</h2>
+    <
+  </div>
+  
 );
 
 export default Home;
