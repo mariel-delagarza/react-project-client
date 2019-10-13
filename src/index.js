9a1f968da96cb0f65f2228e022b1b195dc725fa0
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 20:55:07 -0400

    Import usersReducer and change reducers to reducer inside store, create basic reducer statement

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import { thunk } from 'redux-thunk';
 import { Provider } from 'react-redux';
+import usersReducer from './reducers/users.js';
 
 //Import from app files
 import App from './App';
+
+//Basic Reducer for initial app creation
+const reducer = combineReducers({
+  user: usersReducer
+})
 
 //Install redux devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //Create store to use Redux
 const store = createStore(
-  reducers,
+  reducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
