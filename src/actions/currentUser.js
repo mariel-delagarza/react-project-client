a0adf5ef34abe1694742bfb9101b83c1fb4c6728
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 21:41:39 -0400

    Begin writing login action and separate actions into sync and async groups

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,6 +1,15 @@
+//synchronous action creators
 export const setCurrentUser = user => {
   return {
     type: "SET_CURRENT_USER",
     payload: user
+  }
+}
+
+
+//asynchronous action creators
+export const login = credentials => {
+  return {
+    
   }
 }
