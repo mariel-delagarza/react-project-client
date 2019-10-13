cb5a2eb53fda373e2b674bd685ff30f9dfa5a759
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 22:18:23 -0400

    Add explanatory comments, create mapStateToProps

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Login = () => {
+// Argument could also be "props"
+// and values could be props.username 
+// and props.password
+const Login = ({ username, password}) => {
   return (
     <form onSubmit={undefined}>
-      <input placeholder="username" value={undefined} name="username" type="text" onChange={undefined} />
-      <input placeholder="password" value={undefined} name="password" type="text" onChange={undefined} />
+      <input placeholder="username" value={username} name="username" type="text" onChange={undefined} />
+      <input placeholder="password" value={password} name="password" type="text" onChange={undefined} />
       <input type="submit" value="Log In" />
     </form>
   )
 }
 
-export default Login 
+// Take state from redux, say I need these chunks of state from the store
+// This is read-only; pulling from the store
+// Gives us access to these chunks of state from the store _as props_
+// Props get passed into functional component as an argument&&object
+// This gives me an argument coming to this component that looks like this:
+// {
+//  username: "Sarah1",
+//  password: "password"
+// }
+// Which means I can destructure it out as an argument
+const mapStateToProps = state => {
+  return {
+    username: state.loginForm.username,
+    password: state.loginForm.password
+  }
+}
+
+export default connect(mapStateToProps)(Login) 
