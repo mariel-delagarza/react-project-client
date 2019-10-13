2f830cdf1069d62f453ad9a98b25003131130460
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 22:08:53 -0400

    Temporarily use undefined inside JSX so form will render

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <form onSubmit={}>
-      <input value={} name="username" type="text" onChange={} />
-      <input value={} name="password" type="text" onChange={} />
+    <form onSubmit={undefined}>
+      <input value={undefined} name="username" type="text" onChange={undefined} />
+      <input value={undefined} name="password" type="text" onChange={undefined} />
       <input type="submit" value="Log In" />
     </form>
   )
