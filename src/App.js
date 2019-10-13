202af5a5bc8314e7badad541504965295fa9c0fe
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 23:43:21 -0400

    Import getCurrentUser and invoke on componentDidMount; user in connect as second argument

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Login from './components/Login.js';
+import { getCurrentUser } from './actions/currentUser.js';
 
 class App extends React.Component {
 
   componentDidMount() {
-    
+    getCurrentUser()
   }
 
   render() {
@@ -15,4 +17,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
