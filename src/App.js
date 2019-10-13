8950baad6173ec95ade308c69258caf7a906a5e2
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 23:35:44 -0400

    Setup componentDidMount to be filled in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import Login from './components/Login.js';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
       <Login />
