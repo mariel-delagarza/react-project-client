414772e9b561ee3c8d02fdb08defce09645d781d
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 23:21:31 -0400

    Update login action and add explanatory comments'

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -13,6 +13,7 @@ export const setCurrentUser = user => {
 export const login = credentials => {
   console.log("crendetials are", credentials )
   return dispatch => {
+    //hit the api endpoint and sent information from form
     return fetch("http://localhost:3001/api/v1/login", {
       method: "POST",
       headers: {
@@ -20,5 +21,18 @@ export const login = credentials => {
       },
       body: JSON.stringify(credentials)
     })
+    //take the response from the api and make it json
+    .then(response => response.json())
+    //the user will be returned from the api
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+        //do the set_current_user action
+        //with the payload of user
+        dispatch({type: "SET_CURRENT_USER", user: user})
+      }
+    })
+    .catch()
   }
 }
