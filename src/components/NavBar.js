4714f5c7fba108f821c376e97b6fa6f055c8e1cc
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 16:12:40 -0400

    Add 2 buttons - login and sign up, comment out unneeded code for now, include semantic-ui classNames

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,9 @@ const NavBar = ({ currentUser }) => {
     <div className="ui secondary pointing menu">
       {currentUser ? <p>Welcome, {currentUser.attributes.name}</p> : ""}
       <div className="right menu">
-      {currentUser ? <Logout /> : <Login />}
+        {/*{currentUser ? <Logout /> : <Login />} */}
+        <button className="ui button">Log in</button>
+        <button className="ui button">Sign up</button>
       </div>
     </div>
   )
