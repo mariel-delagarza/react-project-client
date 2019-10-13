b3a0d386339c649f815eaea8c408c539eba57291
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 17:39:49 -0400

    Change Log In submit value to Sign up

---
+++ src/components/Signup.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateSignupForm } from '../actions/signupForm.js';
+import { signup } from '../actions/currentUser.js';
+
+const Signup = ({ signupFormData, updateSignupForm, signup}) => {
+  
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    signup(signupFormData)
+  }
+
+  return (
+    <form className="ui form" onSubmit={handleSubmit}>
+      <input placeholder="username" value={signupFormData.username} name="username" type="text" onChange={handleInputChange} />
+      <input placeholder="password" value={signupFormData.password} name="password" type="text" onChange={handleInputChange} />
+      <input className="ui button" type="submit" value="Sign Up" />
+    </form>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    signupFormData: state.signupForm
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm, signup })(Signup) 
