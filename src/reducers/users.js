97719f72d2d756233ee1eb8d2ddcf518a9fd01df
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 20:51:08 -0400

    Create initial/default reducer for users

---
+++ src/reducers/users.js
@@ -0,0 +1,8 @@
+//File for each reducer and then export that reducer 
+//This reducer is for users so is named users.js
+export default (state = [], action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
