b5ad394a44d899763d09ff5c95d32ce8868cb128
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 17:56:55 -0400

    Create signup route and import component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { Route } from 'react-router-dom';
 import { getCurrentUser } from './actions/currentUser.js';
 import NavBar from './components/NavBar.js';
 import Login from './components/Login.js';
-import Logout from './components/Logout.js';
+//import Logout from './components/Logout.js';
 import myAnswers from './components/MyAnswers.js';
-import MainContainer from './components/MainContainer.js';
+//import MainContainer from './components/MainContainer.js';
+import Signup from './components/Signup.js';
 
 class App extends React.Component {
 
@@ -20,8 +21,9 @@ class App extends React.Component {
       <div className="App">
         <NavBar />
         {/*<MainContainer />*/}
+        <Route exact path='/signup' component={Signup} />
         <Route exact path='/login' component={Login} />
-        <Route exact path='/logout' component={Logout} />
+        {/*<Route exact path='/logout' component={Logout} /> */}
         <Route exact path='/my-answers' component={myAnswers} />
       </div>
     );
