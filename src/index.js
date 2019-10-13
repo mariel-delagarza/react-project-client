de0295f952740cc1b2d810a899e31d201a464431
mariel-delagarza <mariel.code@gmail.com>
2019-10-12 20:41:02 -0400

    Reformat to separate imported packages from imported local files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import { Provider } from 'react-redux';
+
+import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
