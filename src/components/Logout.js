6541ce8518cc37334aad6ac4af53d4bd6cb709a7
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 22:22:26 -0400

    Remove unhelpful comments

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,13 +3,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { logout } from '../actions/currentUser.js';
 
-// Argument could also be "props"
-// and values could be props.username 
-// and props.password.
-// Note updateLoginForm is *not* the same action creator imported
-// but is the "beefed up" redux version that can be used as a callback
-
-// update: props.loginForm is destructured as loginForm
 const Logout = ({ logout, history }) => {
   
   return (
