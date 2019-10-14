0c82a8452e01a684df4bfeb1438d76446e2db27f
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 21:43:29 -0400

    Get LoggedIn and use to display logout button if logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 //import Login from './Login.js';
-//import Logout from './Logout.js';
+import Logout from './Logout.js';
 
-const NavBar = ({ currentUser }) => {
+const NavBar = ({ currentUser, loggedIn }) => {
   
   return (
     <div className="ui secondary pointing menu">
-      {currentUser ? <p>Welcome, {currentUser.attributes.name}</p> : ""}
-      <div className="right menu">
-        {/*{currentUser ? <Logout /> : <Login />} */}
-      </div>
+      <NavLink to="/answers">My Answers</NavLink>
+      <NavLink to="/answers/new">New Answers</NavLink>
+      { loggedIn ? <Logout /> : null }
+      {/*{currentUser ? <p>Welcome, {currentUser.attributes.name}</p> : ""} */}
+      {/* <div className="right menu">
+        {currentUser ? <Logout /> : <Login />} 
+      </div> */}
     </div>
   )
 }
 
 const mapStateToProps = ({ currentUser }) => {
   return {
-    currentUser
+    currentUser,
+    loggedIn: !!currentUser
   }
 }
 
