be1a166a798140a3a16489b05b41ab0c3d84f566
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 21:17:47 -0400

    Add history

--- src/components/NewAnswerForm.js
+++ src/components/NewAnswerForm.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { updateNewAnswerForm } from '../actions/newAnswerForm.js';
 
-const NewAnswerForm = ({ question, topic, source_link, answer }) => {
+const NewAnswerForm = ({ question, topic, source_link, answer, history }) => {
 
   const handleInputChange = (event) => {
     event.preventDefault()
