a8dc1a45229463c8376189a69a196d53c1bbe1ed
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 22:34:55 -0400

    Make formData a const

--- src/actions/newAnswerForm.js
+++ src/actions/newAnswerForm.js
@@ -1,7 +1,8 @@
 // synchronous actions
 export const updateNewAnswerForm = (name, value) => {
+  const formData = { name, value }
   return {
     type: "UPDATE_NEW_ANSWER_FORM",
-    formData: { name, value }
+    formData
   }
 }
