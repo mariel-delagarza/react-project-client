000f2cb6b6d3b3fc7ad56001cd80a6f6f2e221a9
mariel-delagarza <mariel.code@gmail.com>
2019-10-13 20:05:58 -0400

    Copy attributes from schema for reference

--- src/components/NewAnswerForm.js
+++ src/components/NewAnswerForm.js
@@ -5,3 +5,11 @@ const NewAnswerForm = ({}) => {
 };
 
 export default NewTripForm
+
+/*
+Below are what is available for creating an Answer:
+t.string "question"
+t.string "topic"
+t.string "source_link"
+t.text "answer"
+t.integer "user_id" */
