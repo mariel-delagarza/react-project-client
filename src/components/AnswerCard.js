6ff9f278299771cc9af8782ad8993abc6222a03b
mariel-delagarza <mariel.code@gmail.com>
2019-10-14 01:33:13 -0400

    Add attributes as <p> tags

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const AnswerCard = ({answer}) => {
   return (
-    <p>{answer.attributes.question}</p>
+    <div>
+      <p>{answer.attributes.question}</p>
+      <p>{answer.attributes.topic}</p>
+      <p>{answer.attributes.source_link}</p>
+      <p>{answer.attributes.answer_param}</p>
+    </div>
+    
   )
 }
 
